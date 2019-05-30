d70048bab1390026496a75220510af2e6b844bfc
maofei <maofei@rsung.com>
2019-05-30 11:28:51 +0800

    add laravel service provider

---
+++ config/kafka.php
@@ -0,0 +1,35 @@
+<?php
+// 文件 /config/kafka.php
+return [
+    // 默认的生产者配置
+    'producer' => [
+        'broker_list' => [
+            env("ALY_KAFKA_BROKER_LIST"),
+        ],
+        'topic' => env("ALY_KAFKA_TOPIC"),
+        'group_id' => env("ALY_KAFKA_GROUP_ID"),
+        "kafka_options" => [
+            "offset.store.method" => "broker",
+        ],
+    ],
+
+    // 消费者列表
+    'consumer_list' => [
+        'consumer_client_add' => [
+            'broker_list' => [
+                env("ALY_KAFKA_BROKER_LIST"),
+            ],
+            'topic_list'    => [
+                env("ALY_KAFKA_TOPIC"),
+            ],
+            'group_id'      => env("ALY_KAFKA_GROUP_ID"),
+            "kafka_options" => [
+                "offset.store.method" => "broker",
+            ],
+            'timeout_ms'    => 3000,
+            'event_list'    => [
+                // 事件名 => 事件回调函数(必须是静态方法)
+            ],
+        ],
+    ],
+];
